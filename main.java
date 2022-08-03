8534af5f857e452b786ad35173eb26f9af646bef
Jacqueline Nascimento <68693824+jacquelinenascimento@users.noreply.github.com>
2022-08-03 17:48:07 -0300

    Update
    Teste nos métodos criados

--- main.java
+++ main.java
@@ -1,9 +1,22 @@
-
+/**
+ * main
+ */
 public class main {
+
     public static void main(String[] args) {
-        Conta p1 = new Conta();
+        Conta p1 = new Conta(0, null, null, 0, false);
+        Conta p2 = new Conta(0, null, null, 0, false);
+
         p1.setDono("Jacque");
-        p1.setNumConta("123456-0");
+        p1.setNumConta(11111);
+        p1.print();
+
+        p2.setDono("Ueslei");
+        p2.setNumConta(22222);
+        p2.print();
+
+        p1.setStatus(true);
+        p1.depositar(100);
         p1.print();
     }
 }
